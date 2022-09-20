6d67566293e75b833ae234e39db5f8fb2d70e94c
Devonvw <devonvanw@gmail.com>
2022-09-20 15:18:30 +0200

    BaseDAO

---
+++ DAL/BaseDAO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+using MongoDB.Driver;
+using Microsoft.VisualBasic;
+using MongoDB.Bson;
+
+namespace BestelsysteemDAL
+{
+    public abstract class BaseDao
+    {
+        private MongoClient dbClient;
+        private IMongoDatabase db;
+        private IMongoCollection<BsonDocument> collection;
+        public BaseDao()
+        {
+            dbClient = new MongoClient("");
+            db = dbClient.GetDatabase("");
+            db.GetCollection<BsonDocument>("");
+        }
+    }
+}
