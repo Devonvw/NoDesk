01429f8a656bb00913ac2f69520833986cfa8beb
KoenMW <koen.wijchers@gmail.com>
2022-09-22 16:22:35 +0200

    ticketCRUD

--- View/Forms/DashboardForm.Designer.cs
+++ View/Forms/DashboardForm.Designer.cs
@@ -52,17 +52,17 @@
             this.panel1.Controls.Add(this.label1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(812, 41);
+            this.panel1.Size = new System.Drawing.Size(708, 31);
             this.panel1.TabIndex = 1;
             // 
             // button1
             // 
             this.button1.Dock = System.Windows.Forms.DockStyle.Right;
-            this.button1.Location = new System.Drawing.Point(669, 0);
+            this.button1.Location = new System.Drawing.Point(565, 0);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(163, 55);
+            this.button1.Size = new System.Drawing.Size(143, 31);
             this.button1.TabIndex = 1;
             this.button1.Text = "Show List";
             this.button1.UseVisualStyleBackColor = true;
@@ -75,7 +75,7 @@
             this.label1.ForeColor = System.Drawing.Color.Black;
             this.label1.Location = new System.Drawing.Point(0, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(262, 41);
+            this.label1.Size = new System.Drawing.Size(213, 32);
             this.label1.TabIndex = 0;
             this.label1.Text = "Current Incidents";
             // 
@@ -84,10 +84,11 @@
             this.flowLayoutPanel1.Controls.Add(this.panel2);
             this.flowLayoutPanel1.Controls.Add(this.panel3);
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 41);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 31);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Padding = new System.Windows.Forms.Padding(0, 40, 0, 0);
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(812, 859);
+            this.flowLayoutPanel1.Padding = new System.Windows.Forms.Padding(0, 30, 0, 0);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(708, 644);
             this.flowLayoutPanel1.TabIndex = 2;
             // 
             // panel2
@@ -95,10 +96,11 @@
             this.panel2.Controls.Add(this.UnresolvedBar);
             this.panel2.Controls.Add(this.label3);
             this.panel2.Controls.Add(this.label2);
-            this.panel2.Location = new System.Drawing.Point(3, 43);
+            this.panel2.Location = new System.Drawing.Point(3, 32);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Padding = new System.Windows.Forms.Padding(4);
-            this.panel2.Size = new System.Drawing.Size(359, 367);
+            this.panel2.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.panel2.Size = new System.Drawing.Size(314, 275);
             this.panel2.TabIndex = 0;
             this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
             // 
@@ -112,7 +114,8 @@
             this.UnresolvedBar.InnerColor = System.Drawing.SystemColors.Control;
             this.UnresolvedBar.InnerMargin = 2;
             this.UnresolvedBar.InnerWidth = -1;
-            this.UnresolvedBar.Location = new System.Drawing.Point(53, 80);
+            this.UnresolvedBar.Location = new System.Drawing.Point(46, 60);
+            this.UnresolvedBar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.UnresolvedBar.MarqueeAnimationSpeed = 2000;
             this.UnresolvedBar.Name = "UnresolvedBar";
             this.UnresolvedBar.OuterColor = System.Drawing.Color.Silver;
@@ -121,7 +124,7 @@
             this.UnresolvedBar.ProgressColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
             this.UnresolvedBar.ProgressWidth = 22;
             this.UnresolvedBar.SecondaryFont = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.UnresolvedBar.Size = new System.Drawing.Size(247, 237);
+            this.UnresolvedBar.Size = new System.Drawing.Size(216, 178);
             this.UnresolvedBar.StartAngle = 270;
             this.UnresolvedBar.SubscriptColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(166)))), ((int)(((byte)(166)))));
             this.UnresolvedBar.SubscriptMargin = new System.Windows.Forms.Padding(10, -35, 0, 0);
@@ -137,9 +140,9 @@
             // label3
             // 
             this.label3.Dock = System.Windows.Forms.DockStyle.Top;
-            this.label3.Location = new System.Drawing.Point(4, 30);
+            this.label3.Location = new System.Drawing.Point(4, 23);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(351, 15);
+            this.label3.Size = new System.Drawing.Size(306, 11);
             this.label3.TabIndex = 2;
             this.label3.Text = "All tickets currently open";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -148,9 +151,9 @@
             // 
             this.label2.Dock = System.Windows.Forms.DockStyle.Top;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(4, 4);
+            this.label2.Location = new System.Drawing.Point(4, 3);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(351, 26);
+            this.label2.Size = new System.Drawing.Size(306, 20);
             this.label2.TabIndex = 1;
             this.label2.Text = "Unresolved Incidents";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -160,11 +163,11 @@
             this.panel3.Controls.Add(this.deadlineBar);
             this.panel3.Controls.Add(this.label5);
             this.panel3.Controls.Add(this.label4);
-            this.panel3.Location = new System.Drawing.Point(385, 43);
-            this.panel3.Margin = new System.Windows.Forms.Padding(20, 3, 3, 3);
+            this.panel3.Location = new System.Drawing.Point(18, 311);
+            this.panel3.Margin = new System.Windows.Forms.Padding(18, 2, 3, 2);
             this.panel3.Name = "panel3";
-            this.panel3.Padding = new System.Windows.Forms.Padding(4);
-            this.panel3.Size = new System.Drawing.Size(420, 367);
+            this.panel3.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.panel3.Size = new System.Drawing.Size(368, 275);
             this.panel3.TabIndex = 1;
             this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
             // 
@@ -178,7 +181,8 @@
             this.deadlineBar.InnerColor = System.Drawing.SystemColors.Control;
             this.deadlineBar.InnerMargin = 2;
             this.deadlineBar.InnerWidth = -1;
-            this.deadlineBar.Location = new System.Drawing.Point(86, 98);
+            this.deadlineBar.Location = new System.Drawing.Point(75, 74);
+            this.deadlineBar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.deadlineBar.MarqueeAnimationSpeed = 2000;
             this.deadlineBar.Name = "deadlineBar";
             this.deadlineBar.OuterColor = System.Drawing.Color.Silver;
@@ -187,7 +191,7 @@
             this.deadlineBar.ProgressColor = System.Drawing.Color.Red;
             this.deadlineBar.ProgressWidth = 22;
             this.deadlineBar.SecondaryFont = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.deadlineBar.Size = new System.Drawing.Size(247, 237);
+            this.deadlineBar.Size = new System.Drawing.Size(216, 178);
             this.deadlineBar.StartAngle = 270;
             this.deadlineBar.SubscriptColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(166)))), ((int)(((byte)(166)))));
             this.deadlineBar.SubscriptMargin = new System.Windows.Forms.Padding(10, -35, 0, 0);
@@ -203,9 +207,9 @@
             // label5
             // 
             this.label5.Dock = System.Windows.Forms.DockStyle.Top;
-            this.label5.Location = new System.Drawing.Point(4, 30);
+            this.label5.Location = new System.Drawing.Point(4, 23);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(412, 15);
+            this.label5.Size = new System.Drawing.Size(360, 11);
             this.label5.TabIndex = 3;
             this.label5.Text = "These tickets need your immediate attention";
             this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -214,9 +218,9 @@
             // 
             this.label4.Dock = System.Windows.Forms.DockStyle.Top;
             this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label4.Location = new System.Drawing.Point(4, 4);
+            this.label4.Location = new System.Drawing.Point(4, 3);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(412, 26);
+            this.label4.Size = new System.Drawing.Size(360, 20);
             this.label4.TabIndex = 2;
             this.label4.Text = "Incidents past deadline";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -224,14 +228,13 @@
             // 
             // DashboardForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(829, 897);
+            this.ClientSize = new System.Drawing.Size(725, 673);
             this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "DashboardForm";
             this.Text = "DashboardForm";
             this.panel1.ResumeLayout(false);
--- View/Forms/ServiceDaskCRUDTicket.Designer.cs
+++ View/Forms/ServiceDaskCRUDTicket.Designer.cs
@@ -28,19 +28,213 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.createNewIncidentTicketLable = new System.Windows.Forms.Label();
+            this.dateTimeReportedLabel = new System.Windows.Forms.Label();
+            this.subjectOfIincidentLabel = new System.Windows.Forms.Label();
+            this.pryorityLabel = new System.Windows.Forms.Label();
+            this.reportedByUserLabel = new System.Windows.Forms.Label();
+            this.typeOfIncidentLabel = new System.Windows.Forms.Label();
+            this.deadlineFollowUpLabel = new System.Windows.Forms.Label();
+            this.descriptionLabel = new System.Windows.Forms.Label();
+            this.typeOfIncidentCB = new System.Windows.Forms.ComboBox();
+            this.reportedByUserCB = new System.Windows.Forms.ComboBox();
+            this.priorityCB = new System.Windows.Forms.ComboBox();
+            this.subjectOfIncidentTB = new System.Windows.Forms.TextBox();
+            this.descriptionTB = new System.Windows.Forms.TextBox();
+            this.deadLineFollowUpCB = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.SuspendLayout();
+            // 
+            // createNewIncidentTicketLable
+            // 
+            this.createNewIncidentTicketLable.AutoSize = true;
+            this.createNewIncidentTicketLable.Font = new System.Drawing.Font("Arial Narrow", 32.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.createNewIncidentTicketLable.Location = new System.Drawing.Point(34, 47);
+            this.createNewIncidentTicketLable.Name = "createNewIncidentTicketLable";
+            this.createNewIncidentTicketLable.Size = new System.Drawing.Size(464, 52);
+            this.createNewIncidentTicketLable.TabIndex = 0;
+            this.createNewIncidentTicketLable.Text = "Create new incident ticket";
+            // 
+            // dateTimeReportedLabel
+            // 
+            this.dateTimeReportedLabel.AutoSize = true;
+            this.dateTimeReportedLabel.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.dateTimeReportedLabel.Location = new System.Drawing.Point(74, 138);
+            this.dateTimeReportedLabel.Name = "dateTimeReportedLabel";
+            this.dateTimeReportedLabel.Size = new System.Drawing.Size(148, 23);
+            this.dateTimeReportedLabel.TabIndex = 1;
+            this.dateTimeReportedLabel.Text = "Date/time reported : ";
+            // 
+            // subjectOfIincidentLabel
+            // 
+            this.subjectOfIincidentLabel.AutoSize = true;
+            this.subjectOfIincidentLabel.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.subjectOfIincidentLabel.Location = new System.Drawing.Point(74, 178);
+            this.subjectOfIincidentLabel.Name = "subjectOfIincidentLabel";
+            this.subjectOfIincidentLabel.Size = new System.Drawing.Size(146, 23);
+            this.subjectOfIincidentLabel.TabIndex = 2;
+            this.subjectOfIincidentLabel.Text = "Subject of incident : ";
+            // 
+            // pryorityLabel
+            // 
+            this.pryorityLabel.AutoSize = true;
+            this.pryorityLabel.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.pryorityLabel.Location = new System.Drawing.Point(74, 298);
+            this.pryorityLabel.Name = "pryorityLabel";
+            this.pryorityLabel.Size = new System.Drawing.Size(63, 23);
+            this.pryorityLabel.TabIndex = 3;
+            this.pryorityLabel.Text = "Priority :";
+            // 
+            // reportedByUserLabel
+            // 
+            this.reportedByUserLabel.AutoSize = true;
+            this.reportedByUserLabel.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.reportedByUserLabel.Location = new System.Drawing.Point(74, 258);
+            this.reportedByUserLabel.Name = "reportedByUserLabel";
+            this.reportedByUserLabel.Size = new System.Drawing.Size(137, 23);
+            this.reportedByUserLabel.TabIndex = 4;
+            this.reportedByUserLabel.Text = "Reported by user :";
+            // 
+            // typeOfIncidentLabel
+            // 
+            this.typeOfIncidentLabel.AutoSize = true;
+            this.typeOfIncidentLabel.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.typeOfIncidentLabel.Location = new System.Drawing.Point(74, 218);
+            this.typeOfIncidentLabel.Name = "typeOfIncidentLabel";
+            this.typeOfIncidentLabel.Size = new System.Drawing.Size(129, 23);
+            this.typeOfIncidentLabel.TabIndex = 5;
+            this.typeOfIncidentLabel.Text = "Type of incident : ";
+            // 
+            // deadlineFollowUpLabel
+            // 
+            this.deadlineFollowUpLabel.AutoSize = true;
+            this.deadlineFollowUpLabel.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.deadlineFollowUpLabel.Location = new System.Drawing.Point(74, 338);
+            this.deadlineFollowUpLabel.Name = "deadlineFollowUpLabel";
+            this.deadlineFollowUpLabel.Size = new System.Drawing.Size(148, 23);
+            this.deadlineFollowUpLabel.TabIndex = 6;
+            this.deadlineFollowUpLabel.Text = "Deadline/ follow up :";
+            // 
+            // descriptionLabel
+            // 
+            this.descriptionLabel.AutoSize = true;
+            this.descriptionLabel.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.descriptionLabel.Location = new System.Drawing.Point(74, 378);
+            this.descriptionLabel.Name = "descriptionLabel";
+            this.descriptionLabel.Size = new System.Drawing.Size(95, 23);
+            this.descriptionLabel.TabIndex = 7;
+            this.descriptionLabel.Text = "Description :";
+            // 
+            // typeOfIncidentCB
+            // 
+            this.typeOfIncidentCB.FormattingEnabled = true;
+            this.typeOfIncidentCB.Items.AddRange(new object[] {
+            "Software",
+            "HardWare",
+            "Service"});
+            this.typeOfIncidentCB.Location = new System.Drawing.Point(218, 218);
+            this.typeOfIncidentCB.Name = "typeOfIncidentCB";
+            this.typeOfIncidentCB.Size = new System.Drawing.Size(300, 23);
+            this.typeOfIncidentCB.TabIndex = 8;
+            // 
+            // reportedByUserCB
+            // 
+            this.reportedByUserCB.FormattingEnabled = true;
+            this.reportedByUserCB.Location = new System.Drawing.Point(218, 258);
+            this.reportedByUserCB.Name = "reportedByUserCB";
+            this.reportedByUserCB.Size = new System.Drawing.Size(300, 23);
+            this.reportedByUserCB.TabIndex = 9;
+            // 
+            // priorityCB
+            // 
+            this.priorityCB.FormattingEnabled = true;
+            this.priorityCB.Items.AddRange(new object[] {
+            "Low",
+            "Normal",
+            "High"});
+            this.priorityCB.Location = new System.Drawing.Point(218, 298);
+            this.priorityCB.Name = "priorityCB";
+            this.priorityCB.Size = new System.Drawing.Size(300, 23);
+            this.priorityCB.TabIndex = 10;
+            // 
+            // subjectOfIncidentTB
+            // 
+            this.subjectOfIncidentTB.Location = new System.Drawing.Point(218, 178);
+            this.subjectOfIncidentTB.Name = "subjectOfIncidentTB";
+            this.subjectOfIncidentTB.Size = new System.Drawing.Size(300, 23);
+            this.subjectOfIncidentTB.TabIndex = 11;
+            // 
+            // descriptionTB
+            // 
+            this.descriptionTB.Location = new System.Drawing.Point(218, 378);
+            this.descriptionTB.Name = "descriptionTB";
+            this.descriptionTB.Size = new System.Drawing.Size(300, 23);
+            this.descriptionTB.TabIndex = 12;
+            // 
+            // deadLineFollowUpCB
+            // 
+            this.deadLineFollowUpCB.FormattingEnabled = true;
+            this.deadLineFollowUpCB.Items.AddRange(new object[] {
+            "7 days",
+            "14 days",
+            "28 days",
+            "6 months"});
+            this.deadLineFollowUpCB.Location = new System.Drawing.Point(218, 338);
+            this.deadLineFollowUpCB.Name = "deadLineFollowUpCB";
+            this.deadLineFollowUpCB.Size = new System.Drawing.Size(300, 23);
+            this.deadLineFollowUpCB.TabIndex = 13;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(218, 138);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(300, 23);
+            this.dateTimePicker1.TabIndex = 14;
             // 
             // ServiceDaskCRUDTicket
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(873, 482);
+            this.ClientSize = new System.Drawing.Size(774, 520);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.deadLineFollowUpCB);
+            this.Controls.Add(this.descriptionTB);
+            this.Controls.Add(this.subjectOfIncidentTB);
+            this.Controls.Add(this.priorityCB);
+            this.Controls.Add(this.reportedByUserCB);
+            this.Controls.Add(this.typeOfIncidentCB);
+            this.Controls.Add(this.descriptionLabel);
+            this.Controls.Add(this.deadlineFollowUpLabel);
+            this.Controls.Add(this.typeOfIncidentLabel);
+            this.Controls.Add(this.reportedByUserLabel);
+            this.Controls.Add(this.pryorityLabel);
+            this.Controls.Add(this.subjectOfIincidentLabel);
+            this.Controls.Add(this.dateTimeReportedLabel);
+            this.Controls.Add(this.createNewIncidentTicketLable);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "ServiceDaskCRUDTicket";
             this.Text = "ServiceDaskCRUDTicket";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Label createNewIncidentTicketLable;
+        private Label dateTimeReportedLabel;
+        private Label subjectOfIincidentLabel;
+        private Label pryorityLabel;
+        private Label reportedByUserLabel;
+        private Label typeOfIncidentLabel;
+        private Label deadlineFollowUpLabel;
+        private Label descriptionLabel;
+        private ComboBox typeOfIncidentCB;
+        private ComboBox reportedByUserCB;
+        private ComboBox priorityCB;
+        private TextBox subjectOfIncidentTB;
+        private TextBox descriptionTB;
+        private ComboBox deadLineFollowUpCB;
+        private DateTimePicker dateTimePicker1;
     }
 }
