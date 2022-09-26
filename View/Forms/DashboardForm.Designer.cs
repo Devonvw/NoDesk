dc57606373a51ff72598ee224ea953f4e39bb2fb
Devonvw <devonvanw@gmail.com>
2022-09-26 14:28:35 +0200

    Fix basedao

--- View/Forms/DashboardForm.Designer.cs
+++ View/Forms/DashboardForm.Designer.cs
@@ -52,18 +52,17 @@
             this.panel1.Controls.Add(this.label1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1055, 51);
+            this.panel1.Size = new System.Drawing.Size(740, 31);
             this.panel1.TabIndex = 1;
             // 
             // button1
             // 
             this.button1.Dock = System.Windows.Forms.DockStyle.Right;
-            this.button1.Location = new System.Drawing.Point(851, 0);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Location = new System.Drawing.Point(597, 0);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(204, 51);
+            this.button1.Size = new System.Drawing.Size(143, 31);
             this.button1.TabIndex = 1;
             this.button1.Text = "Show List";
             this.button1.UseVisualStyleBackColor = true;
@@ -75,9 +74,8 @@
             this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label1.ForeColor = System.Drawing.Color.Black;
             this.label1.Location = new System.Drawing.Point(0, 0);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(310, 48);
+            this.label1.Size = new System.Drawing.Size(213, 32);
             this.label1.TabIndex = 0;
             this.label1.Text = "Current Incidents";
             // 
@@ -86,11 +84,11 @@
             this.flowLayoutPanel1.Controls.Add(this.pnlUnresolved);
             this.flowLayoutPanel1.Controls.Add(this.pnlDeadline);
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 51);
-            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(4);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 31);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Padding = new System.Windows.Forms.Padding(0, 50, 0, 0);
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(1055, 1074);
+            this.flowLayoutPanel1.Padding = new System.Windows.Forms.Padding(0, 30, 0, 0);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(740, 644);
             this.flowLayoutPanel1.TabIndex = 2;
             // 
             // pnlUnresolved
@@ -98,11 +96,11 @@
             this.pnlUnresolved.Controls.Add(this.unresolvedBar);
             this.pnlUnresolved.Controls.Add(this.label3);
             this.pnlUnresolved.Controls.Add(this.label2);
-            this.pnlUnresolved.Location = new System.Drawing.Point(4, 54);
-            this.pnlUnresolved.Margin = new System.Windows.Forms.Padding(4);
+            this.pnlUnresolved.Location = new System.Drawing.Point(3, 32);
+            this.pnlUnresolved.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pnlUnresolved.Name = "pnlUnresolved";
-            this.pnlUnresolved.Padding = new System.Windows.Forms.Padding(5);
-            this.pnlUnresolved.Size = new System.Drawing.Size(449, 459);
+            this.pnlUnresolved.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pnlUnresolved.Size = new System.Drawing.Size(314, 275);
             this.pnlUnresolved.TabIndex = 0;
             // 
             // unresolvedBar
@@ -116,12 +114,13 @@
             this.unresolvedBar.GradientMode = System.Drawing.Drawing2D.LinearGradientMode.ForwardDiagonal;
             this.unresolvedBar.LineColor = System.Drawing.Color.DimGray;
             this.unresolvedBar.LineWidth = 1;
-            this.unresolvedBar.Location = new System.Drawing.Point(75, 119);
+            this.unresolvedBar.Location = new System.Drawing.Point(52, 71);
+            this.unresolvedBar.Margin = new System.Windows.Forms.Padding(2);
             this.unresolvedBar.Maximum = ((long)(100));
-            this.unresolvedBar.MinimumSize = new System.Drawing.Size(100, 100);
+            this.unresolvedBar.MinimumSize = new System.Drawing.Size(70, 60);
             this.unresolvedBar.Name = "unresolvedBar";
             this.unresolvedBar.ProgressShape = View.ExtraControl.CircularProgressBar._ProgressShape.Round;
-            this.unresolvedBar.Size = new System.Drawing.Size(285, 285);
+            this.unresolvedBar.Size = new System.Drawing.Size(200, 200);
             this.unresolvedBar.TabIndex = 3;
             this.unresolvedBar.Text = "57";
             this.unresolvedBar.TextMode = View.ExtraControl.CircularProgressBar._TextMode.Custom;
@@ -130,10 +129,9 @@
             // label3
             // 
             this.label3.Dock = System.Windows.Forms.DockStyle.Top;
-            this.label3.Location = new System.Drawing.Point(5, 37);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(4, 22);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(439, 30);
+            this.label3.Size = new System.Drawing.Size(306, 18);
             this.label3.TabIndex = 2;
             this.label3.Text = "All tickets currently open";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -142,10 +140,9 @@
             // 
             this.label2.Dock = System.Windows.Forms.DockStyle.Top;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(5, 5);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(4, 3);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(439, 32);
+            this.label2.Size = new System.Drawing.Size(306, 19);
             this.label2.TabIndex = 1;
             this.label2.Text = "Unresolved Incidents";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -155,11 +152,11 @@
             this.pnlDeadline.Controls.Add(this.deadlineBar);
             this.pnlDeadline.Controls.Add(this.label5);
             this.pnlDeadline.Controls.Add(this.label4);
-            this.pnlDeadline.Location = new System.Drawing.Point(482, 54);
-            this.pnlDeadline.Margin = new System.Windows.Forms.Padding(25, 4, 4, 4);
+            this.pnlDeadline.Location = new System.Drawing.Point(338, 32);
+            this.pnlDeadline.Margin = new System.Windows.Forms.Padding(18, 2, 3, 2);
             this.pnlDeadline.Name = "pnlDeadline";
-            this.pnlDeadline.Padding = new System.Windows.Forms.Padding(5);
-            this.pnlDeadline.Size = new System.Drawing.Size(525, 459);
+            this.pnlDeadline.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pnlDeadline.Size = new System.Drawing.Size(368, 275);
             this.pnlDeadline.TabIndex = 1;
             // 
             // deadlineBar
@@ -173,12 +170,13 @@
             this.deadlineBar.GradientMode = System.Drawing.Drawing2D.LinearGradientMode.ForwardDiagonal;
             this.deadlineBar.LineColor = System.Drawing.Color.DimGray;
             this.deadlineBar.LineWidth = 1;
-            this.deadlineBar.Location = new System.Drawing.Point(101, 137);
+            this.deadlineBar.Location = new System.Drawing.Point(71, 82);
+            this.deadlineBar.Margin = new System.Windows.Forms.Padding(2);
             this.deadlineBar.Maximum = ((long)(100));
-            this.deadlineBar.MinimumSize = new System.Drawing.Size(100, 100);
+            this.deadlineBar.MinimumSize = new System.Drawing.Size(70, 60);
             this.deadlineBar.Name = "deadlineBar";
             this.deadlineBar.ProgressShape = View.ExtraControl.CircularProgressBar._ProgressShape.Round;
-            this.deadlineBar.Size = new System.Drawing.Size(285, 285);
+            this.deadlineBar.Size = new System.Drawing.Size(200, 200);
             this.deadlineBar.TabIndex = 4;
             this.deadlineBar.Text = "57";
             this.deadlineBar.TextMode = View.ExtraControl.CircularProgressBar._TextMode.Custom;
@@ -187,10 +185,9 @@
             // label5
             // 
             this.label5.Dock = System.Windows.Forms.DockStyle.Top;
-            this.label5.Location = new System.Drawing.Point(5, 37);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Location = new System.Drawing.Point(4, 22);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(515, 30);
+            this.label5.Size = new System.Drawing.Size(360, 18);
             this.label5.TabIndex = 3;
             this.label5.Text = "These tickets need your immediate attention";
             this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -199,24 +196,22 @@
             // 
             this.label4.Dock = System.Windows.Forms.DockStyle.Top;
             this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label4.Location = new System.Drawing.Point(5, 5);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(4, 3);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(515, 32);
+            this.label4.Size = new System.Drawing.Size(360, 19);
             this.label4.TabIndex = 2;
             this.label4.Text = "Incidents past deadline";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // DashboardForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(1081, 1121);
+            this.ClientSize = new System.Drawing.Size(757, 556);
             this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "DashboardForm";
             this.Text = "DashboardForm";
             this.panel1.ResumeLayout(false);
