28aff6091f3c57ccf33ef852f34517174bccc4b6
muttalipSahin <muttalip9801@gmail.com>
2022-09-29 01:36:07 +0200

    basedoa

---
+++ DAL/BaseDAO (1).cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+using MongoDB.Driver;
+using Microsoft.VisualBasic;
+using MongoDB.Bson;
+
+namespace DAL
+{
+    public abstract class BaseDao
+    {
+        private MongoClient dbClient;
+        protected IMongoDatabase db;
+        protected IMongoCollection<BsonDocument> collection;
+        public BaseDao()
+        {
+            dbClient = new MongoClient("mongodb+srv://KoenWijchers:nosql@cluster0.fb34nwv.mongodb.net/?retryWrites=true&w=majority");
+            db = dbClient.GetDatabase("NoDesk");
+            db.GetCollection<BsonDocument>("Tickets");
+        }
+    }
+}
