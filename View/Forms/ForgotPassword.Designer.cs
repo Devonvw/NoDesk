b1e141a359d1aef5f8415b1dce36588209cac19a
Esadullah1 <45165761+Esadullah1@users.noreply.github.com>
2022-10-08 22:12:37 +0200

    fix

--- View/Forms/ForgotPassword.Designer.cs
+++ View/Forms/ForgotPassword.Designer.cs
@@ -61,13 +61,15 @@
             // 
             // button1
             // 
-            this.button1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
             this.button1.Location = new System.Drawing.Point(265, 294);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(204, 33);
             this.button1.TabIndex = 9;
             this.button1.Text = "reset password";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // label3
             // 
